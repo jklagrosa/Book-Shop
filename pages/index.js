647368c031da394a6de87408f515f6f0d97ae6e5
jklagrosa <jklagrosa.dev@gmail.com>
2022-05-18 19:15:56 +0800

    Added another h1 tag

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 style={{ color: "red" }}>BOOK SHOP WEB APPLICATION</h1>
-      <p>
-        Fugiat aliqua sunt voluptate cupidatat reprehenderit ad sunt id. Laborum
-        nostrud labore in ipsum. Quis excepteur anim et qui nostrud irure ea ex
-        ad ullamco eu et eu.
-      </p>
-      <p>
-        Consequat adipisicing deserunt quis voluptate ad aute eu velit fugiat
-        sunt do proident tempor. Proident qui magna nisi adipisicing tempor
-        culpa. Voluptate adipisicing ex aute id. Fugiat aute proident pariatur
-        et.
-      </p>
+      <h1 style={{ color: "blue" }}>ADDED ANOTHER H1 TAG!</h1>
     </>
   );
 }
