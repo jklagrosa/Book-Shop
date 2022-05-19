d7a85346810da8459641756385c4a03ed14674e8
jklagrosa <jklagrosa.dev@gmail.com>
2022-05-19 17:13:31 +0800

    changes made

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,16 @@ const Hero = () => {
   return (
     <>
       <div className={styles.Wrapper}>
-        <h1>Books are not only to entertain us but also inspire us.</h1>
+        <h1>
+          <span>Books</span> are not only to <span>Entertain</span> us but also{" "}
+          <span>Inspire</span> us.
+        </h1>
+
+        <div className={styles.hero_btn_wrapper}>
+          <button>Featured Books</button>
+          <span className={styles.span_divider}></span>
+          <button>Book Sale</button>
+        </div>
       </div>
     </>
   );
