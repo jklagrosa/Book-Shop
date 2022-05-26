3522bc1f5297992ef2bc9d767bb284cb4aa5a3f7
jklagrosa <jklagrosa.dev@gmail.com>
2022-05-26 22:08:49 +0800

    [FINISHED] Dynamic book page

--- pages/book-details/[id].js
+++ pages/book-details/[id].js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import dbConnection from "../../utils/conn";
+import Books from "../../models/books";
 import Navbar from "../../components/Navbar";
 import NavLinks from "../../components/NavLinks";
 import Footer from "../../components/Footer";
@@ -14,76 +17,165 @@ import {
   MdFavorite,
 } from "react-icons/md";
 
-const BookId = () => {
+import { useSelector } from "react-redux";
+
+export async function getStaticPaths() {
+  await dbConnection();
+  const get_id = await Books.find({});
+  const paths = get_id.map((x) => {
+    return {
+      params: { id: `${x._id}` },
+    };
+  });
+  console.log(paths);
+  if (!get_id) {
+    return {
+      paths: [],
+      fallback: false,
+    };
+  }
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  await dbConnection();
+  const { params } = context;
+  const find_book = await Books.findOne({ _id: `${params.id}` });
+  const get_books_for_left_display = await Books.find({}).limit(8);
+
+  if (!find_book) {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+
+  if (!get_books_for_left_display) {
+    return {
+      props: {
+        display: null,
+      },
+    };
+  }
+
+  console.log(find_book);
+
+  return {
+    props: {
+      data: JSON.stringify(find_book),
+      display: JSON.stringify(get_books_for_left_display),
+    },
+  };
+}
+
+const BookId = ({ data, display }) => {
+  const [books, setBooks] = useState([]);
+  const [display_books, setDisplay_Books] = useState([]);
+  const [HAS_BOOKS, SET_HAS_BOOKS] = useState(true);
+  const [HAS_DISPLAY, SET_HAS_DISPLAY] = useState(true);
+  const parsed_data = data ? JSON.parse(data) : false;
+  const parsed_display = display ? JSON.parse(display) : false;
+
+  useEffect(() => {
+    if (!parsed_data) {
+      setBooks([]);
+      SET_HAS_BOOKS(false);
+    } else {
+      setBooks(parsed_data);
+      SET_HAS_BOOKS(true);
+      console.log(books);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!parsed_display) {
+      setDisplay_Books([]);
+      SET_HAS_DISPLAY(false);
+    } else {
+      setDisplay_Books(parsed_display);
+      SET_HAS_DISPLAY(true);
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
       <NavLinks />
-      <div className={styles.Wrapper}>
-        <Container fluid="lg">
-          <Row className="gy-0 gx-4" id={styles.row_wrapper}>
-            <Col xs={12} md={8}>
-              <div className={styles.book_details_wrapper}>
-                <img src="/books/b1.jpg" />
-                <div className={styles.stack_wrapper}>
-                  <MdFavoriteBorder
-                    className={styles.icons}
-                    style={{ color: "#f2b195", cursor: "pointer" }}
-                  />
-                  <span className="mx-2"></span>
-                  <BsCart2
-                    className={styles.icons}
-                    style={{ color: "#12a4d9", cursor: "pointer" }}
-                  />
 
-                  <h3>Adventure Fiction</h3>
-                  <h1>Lorem Silum and Sopas Kabra. </h1>
+      {HAS_BOOKS && (
+        <div className={styles.Wrapper}>
+          <Container fluid="lg">
+            <Row className="gy-0 gx-4" id={styles.row_wrapper}>
+              <Col xs={12} md={8}>
+                <div className={styles.book_details_wrapper}>
+                  <img src={`/books/${books.img}`} />
+                  <div className={styles.stack_wrapper}>
+                    <MdFavoriteBorder
+                      className={styles.icons}
+                      style={{ color: "#f2b195", cursor: "pointer" }}
+                    />
+                    <span className="mx-2"></span>
+                    <BsCart2
+                      className={styles.icons}
+                      style={{ color: "#12a4d9", cursor: "pointer" }}
+                    />
 
-                  <MdPersonOutline className={styles.icons_others} />
-                  <span className={styles.book_others}>Pablo Escovar</span>
-                  <br></br>
-                  <MdStarOutline className={styles.icons_others} />
-                  <span className={styles.book_others}>4.5/5</span>
+                    <h3>{books.genre}</h3>
+                    <h1>{books.title}</h1>
 
-                  <h2>
-                    <span style={{ textDecoration: "line-through" }}>
-                      ₱ 100
-                    </span>{" "}
-                    ₱ 300
-                  </h2>
-                  <p>
-                    Ad esse eu sunt sit ad eu elit sunt dolore cillum. Dolore
-                    duis enim veniam laborum adipisicing proident incididunt
-                    sint. Non veniam minim in nulla quis proident adipisicing
-                    anim qui voluptate eu proident velit incididunt. Et cillum
-                    Lorem velit laborum pariatur dolore voluptate adipisicing.
-                    Exercitation cillum anim incididunt laborum. Laborum tempor
-                    dolore cillum ad labore et voluptate aute. Officia mollit
-                    amet dolor nostrud nostrud non cillum.
-                  </p>
+                    <MdPersonOutline className={styles.icons_others} />
+                    <span className={styles.book_others}>{books.author}</span>
+                    <br></br>
+                    <MdStarOutline className={styles.icons_others} />
+                    <span className={styles.book_others}>
+                      {books.ratings}/5
+                    </span>
+
+                    <h2>
+                      {books.prevPrice && (
+                        <>
+                          <span style={{ textDecoration: "line-through" }}>
+                            ₱{books.prevPrice}
+                          </span>
+                          {" / "}
+                        </>
+                      )}
+                      ₱{books.price}
+                    </h2>
+                    <p>{books.desc}</p>
+                  </div>
                 </div>
-              </div>
-            </Col>
-            <Col xs={12} md={4}>
-              <div className={styles.display_other_books}>
-                <h4>Select other books</h4>
-                <hr className={styles.divider} />
-                <div className={styles.display_other_books_wrapper}>
-                  <h5>Lorem silum and Sopas Kabra</h5>
-                  <h6>Pablo Escobar</h6>
-                  <h6>4.5/5</h6>
-                </div>
+              </Col>
+              <Col xs={12} md={4}>
+                {HAS_DISPLAY && (
+                  <div className={styles.display_other_books}>
+                    <h4>Select other books</h4>
+                    <hr className={styles.divider} />
 
-                <div className={styles.display_other_books_wrapper}>
-                  <h5>Lorem silum and Sopas Kabra</h5>
-                  <h6>Pablo Escobar</h6>
-                  <h6>4.5/5</h6>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+                    {display_books.map((x) => (
+                      <div
+                        className={styles.display_other_books_wrapper}
+                        key={x._id}
+                      >
+                        <h5>{x.title}</h5>
+                        <h6>{x.author}</h6>
+                        <h6>{x.ratings}/5</h6>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
+
+      {!HAS_BOOKS && <h1>NO BOOKS TO SHOW</h1>}
 
       <Footer />
       <Copyright />
